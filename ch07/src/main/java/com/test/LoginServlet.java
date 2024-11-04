f67b6e04dfc2f7faedaca76625e5f0de80c4f1a8
baek0816 <2440108@scjc.ac.kr>
2024-11-04 16:45:21 +0900

    jsp ch07

---
+++ ch07/src/main/java/com/test/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uid = request.getParameter("id");
+        String ups = request.getParameter("ps");
+
+        // HTML 문서를 한번에 작성
+        String res = "<html><body>"
+                   + "<h3>User ID: " + uid + "</h3>"
+                   + "<h3>User PS: " + ups + "</h3>"
+                   + "</body></html>";
+        response.getWriter().print(res);
+    }
+    
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
