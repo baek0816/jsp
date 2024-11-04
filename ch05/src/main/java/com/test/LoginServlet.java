8b2d4959d18a08ed955227cebcf7102919746b4c
baek0816 <2440108@scjc.ac.kr>
2024-11-04 16:44:59 +0900

    jsp ch05

---
+++ ch05/src/main/java/com/test/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uid = request.getParameter("id");
+		String res = "<html><h3>User ID: " + uid + "</h3></html>";
+		response.getWriter().print(res);
+		String ups = request.getParameter("ps");
+		String res2 = "<html><h3>User PS: " + uid + "</h3></html>";
+		response.getWriter().print(res2);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
